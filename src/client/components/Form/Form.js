e17034d79c060a6b434cfe7c814752f1dbda8686
chrisjkellett <cjkellett@hotmail.co.uk>
2019-05-14 20:07:58 +0200

    removed ids from options in form as not used

--- src/client/components/Form/Form.js
+++ src/client/components/Form/Form.js
@@ -8,9 +8,9 @@ export default function Form(props) {
       <textarea id="text" onChange={props.change}/>
       <button onClick={props.submit}>submit</button>
       <select id="level" onChange={props.change}>
-        <option id="0">easy</option>
-        <option id="1">intermediate</option>
-        <option id="2">hard</option>
+        <option>easy</option>
+        <option>intermediate</option>
+        <option>hard</option>
       </select>
       <Error showError={props.showError} minimumWords={props.minimumWords} />
     </div> 
