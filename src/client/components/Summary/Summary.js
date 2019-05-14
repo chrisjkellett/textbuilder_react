994ed57e247469188da4c50e70c3ea4337fc8bf9
chrisjkellett <cjkellett@hotmail.co.uk>
2019-05-14 20:40:15 +0200

    added styles to Summary cmp

--- src/client/components/Summary/Summary.js
+++ src/client/components/Summary/Summary.js
@@ -1,17 +1,19 @@
 import React from 'react'
 
 export default function Summary(props) {
-  console.log(props.corrections);
-  console.log(props.text);
-  console.log(props.userAnswers);
   return (
     <div>
       {props.corrections.map((word, index) => {
         if(word[0] === '#')
           return <span className='passed' key={index}>{props.text[index]}</span>
-        else if(word[0] === '!')
-          return <span className='failed' key={index}>{props.userAnswers[index]}</span>
-        else
+        else if(word[0] === '!'){
+          return (
+            <span>
+              <span className='failed' key={index}>{props.userAnswers[index]}</span>
+              <span>({props.text[index]})</span>
+            </span>
+          )
+        }else
           return <span key={index}>{word}</span>
       })}
     </div>
