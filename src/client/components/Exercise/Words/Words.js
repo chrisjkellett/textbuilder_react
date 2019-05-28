fb59c45cb26a86f2d532486572e3c8f752ef0926
chrisjkellett <cjkellett@hotmail.co.uk>
2019-05-28 16:03:46 +0200

    each word renders a div instead of a span with proper display props to avoid texts running off screen in Exercise

--- src/client/components/Exercise/Words/Words.js
+++ src/client/components/Exercise/Words/Words.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import styles from './Words.module.css';
 
 export default function Words(props) {
   return props.exercise.map((word, i) => {
     if(word !== '{gap}')
-      return <span key={i} id={i}>{word}</span>
+      return <div className={styles.Word} key={i} id={i}>{word}</div>
     else
       return <input onChange={props.change} value={props.userAnswers[i]} key={i} id={i} />
   });
-}
+};
