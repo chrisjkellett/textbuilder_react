4e3ce28896d667ef7213f41656d010927d87b310
chrisjkellett <cjkellett@hotmail.co.uk>
2019-05-28 16:05:19 +0200

    removed styles from css modules and added to /css

--- src/client/components/Exercise/Words/Words.js
+++ src/client/components/Exercise/Words/Words.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import styles from './Words.module.css';
 
 export default function Words(props) {
   return props.exercise.map((word, i) => {
     if(word !== '{gap}')
-      return <div className={styles.Word} key={i} id={i}>{word}</div>
+      return <div className="Word" key={i} id={i}>{word}</div>
     else
       return <input onChange={props.change} value={props.userAnswers[i]} key={i} id={i} />
   });
